976d3e597ea7ec5812210367c93506558ac9d419
Ali Mostafa <averagelua@gmail.com>
2024-10-14 15:20:50 -0500

    update transcription

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from faster_whisper import WhisperModel
 import os
 
 model_size = "small.en"
-model = WhisperModel(model_size, device="cpu")
+model = WhisperModel(model_size, device="cpu", num_workers=os.cpu_count())
 
 
 def audio_callback(file_name):
-    segments, info = model.transcribe(file_name, beam_size=5, language="en")
+    segments, info = model.transcribe(
+        file_name, beam_size=5, language="en", vad_filter=True)
+
     text = ""
     for segment in segments:
         text += segment.text
